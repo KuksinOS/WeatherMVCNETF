27243e027e85809ad7eb219c908ec265bad8f13d
alekskakuk <alekskakuk@gmail.com>
2020-03-13 15:54:07 +0200

    adding where graphql

---
+++ WeatherMVCNETF.Library/Interfaces/ICityEndPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMVCNETF.Library.Interfaces
+{
+    public interface ICityEndPoint
+    {
+        Task<List<WeatherMVCNETF.Library.Models.ISO3166CountryCodes.Result>> GetAll();
+    }
+}
